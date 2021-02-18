74a3f296a1a744512da4b8aa23c01725f50addc3
Reinaldo Dos Santos <reinalddos@gmail.com>
2021-02-18 12:50:46 +0100

    change serial interval

--- Shiny doc/Epidemio.R
+++ Shiny doc/Epidemio.R
@@ -4,15 +4,28 @@ Incidences =
   split(f = .$iso3c) %>% 
   map(.f = Calcul_incidence)
 
+get_estimate = ~EpiEstim::estimate_R(incid = .,
+                                     method = "parametric_si",
+                                     config = make_config(list(mean_si = 3.96,
+                                                               std_si = 4.75)))
+
 estimateR =
   Incidences %>%
-  map(.f = EpiEstim::estimate_R,
-      method = "parametric_si",
-      config = make_config(list(mean_si = 5.71,
-                                std_si = 3.89)))
+  map(.f = safely(get_estimate))
 
-Incidences_current_wave =
-  Daily_data %>%
-  split(f = .$iso3c) %>% 
-  map(.f = Deuxieme_vague) %>%
-  map(.f = Calcul_incidence)
+estimateR = estimateR %>% purrr::transpose() %$% result %>% compact()%>%
+  map(.f = Get_weekly_R0) %>%
+  bind_rows(.id = "iso3c") %>%
+  left_join(y = Daily_data %>% distinct(iso3c, country),
+            by = "iso3c")
+
+PLOTT =
+  Panel_data %>% 
+  filter(iso3c=="FRA") %>% distinct(region, income) %>% 
+  semi_join(x= Panel_data) %>% distinct(iso3c) %>% 
+  semi_join(x=estimateR)  %>% 
+  ggplot(mapping = aes(x = Date, y = `Median(R)`)) +
+  geom_line(mapping = aes(colour = country)) +
+  geom_hline(yintercept = 1, colour = "red")
+
+plotly::ggplotly(dynamicTicks = T, p = PLOTT)
